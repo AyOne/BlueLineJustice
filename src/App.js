7d139192a065a44391248b7935067ae1fe217177
AyOne <bettinger.gaspard@gmail.com>
2023-05-31 04:51:02 +0200

    proper Hello World

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import LawerCall from "./pages/LawyerCall/LawyerCall";
+import { Box, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+
+
+
+
+
+
+export default function App() {
+
+
+
+	//https://bareynol.github.io/mui-theme-creator/
+	const darkTheme = createTheme({
+		palette: {
+			mode: 'dark',
+			primary: {
+				main: '#6170c5',
+			},
+			secondary: {
+				main: '#f50057',
+			},
+			background: {
+				default:"#303030",
+				paper:"#424242"
+			}
+		},
+	});
+
+	const lightTheme = createTheme({
+		palette:{
+			mode:"light",
+		},
+	});
+
+
+
+
+	const urlToView = () => {
+		const url = window.location.pathname;
+		switch (url) {
+			case "/":
+				return <LawerCall />
+			default:
+				return <h1>404</h1>
+		}
+	}
+
+	const isUrlDark = () => {
+		const url = window.location.pathname;
+		switch (url) {
+			case "/":
+				return true;
+			default:
+				return false;
+		}
+	}
+
+
+	return (
+		<ThemeProvider theme={isUrlDark() ? darkTheme : lightTheme}>
+			<CssBaseline />
+			<Box sx={{
+				width:"100vw",
+        		height:"100vh",
+				overflowX:"hidden",
+				overflowY:"scroll",
+				display:"flex",
+				flexDirection:"column"
+			}}>
+				<Box sx={{
+					width:1,
+					flexGrow:1,
+				}}>
+					{urlToView()}
+				</Box>
+			</Box>
+		</ThemeProvider>
+	)
+
+
+
+
+
+
+
+
+
 }
-
-export default App;
---
+++ src/pages/LawyerCall/LawyerCall.js
@@ -0,0 +1,21 @@
+
+
+
+import { Box, Typography } from '@mui/material';
+
+
+
+export default function LawerCall(props) {
+	return (
+		<Box sx={{
+			display:"flex",
+			alignItems:"center",
+			justifyContent:"center",
+			align:"center",
+			maxWidth:"80%",
+			margin:"5% auto 5% auto"
+		}}>
+			<Typography variant="h3">LawyerCall</Typography>
+		</Box>
+	)
+}
